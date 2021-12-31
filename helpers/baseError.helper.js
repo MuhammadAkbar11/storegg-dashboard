31e168319a3beaf79066bf229d1d0929d820c5bb
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2021-12-31 14:46:29 +0700

    create error helper dan delete utils error

--- helpers/baseError.helper.js
+++ helpers/baseError.helper.js
@@ -1,25 +1,25 @@
 class BaseError extends Error {
-  constructor(name, statusCode, description, isOperational, data) {
+  constructor(
+    name,
+    statusCode,
+    description,
+    isOperational = true,
+    errors = {}
+  ) {
     super(description);
 
     Object.setPrototypeOf(this, new.target.prototype);
+
     this.name = name;
     this.statusCode = statusCode;
-    this.data = data;
     this.description = description;
     this.isOperational = isOperational;
     this.stack;
-    this.responseType;
-    Error.captureStackTrace(this);
-  }
-
-  render(view = "errors/500") {
     this.responseType = "page";
-    this.errorView = view;
-  }
-
-  json() {
-    this.responseType = "json";
+    Object.keys(errors).map(err => {
+      this[err] = errors[err];
+    });
+    Error.captureStackTrace(this);
   }
 }
 
