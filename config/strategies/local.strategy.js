969b058394aa1be2c8ce064f2a52aef606f858d2
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2022-01-04 13:45:39 +0700

    error handling for local strategy

--- config/strategies/local.strategy.js
+++ config/strategies/local.strategy.js
@@ -1,7 +1,8 @@
 import { Strategy } from "passport-local";
+import BaseError, { TransfromError } from "../../helpers/baseError.helper.js";
 import UserModel from "../../models/User.model.js";
 
-const localStrategy = new Strategy(
+const LocalStrategy = new Strategy(
   {
     usernameField: "email",
     passwordField: "password",
@@ -12,11 +13,21 @@ const localStrategy = new Strategy(
       if (user) {
         return done(null, user);
       }
-      throw new Error("Authentication Failed");
-    } catch (error) {
-      done(error);
+      throw new BaseError("Authentication", "Failed to Login", 400, true, {
+        errorView: "auth/login",
+      });
+    } catch (err) {
+      const error = new BaseError(
+        "Authentication",
+        err?.message || "Failed to Login",
+        err?.status || 400,
+        true,
+        { ...err }
+      );
+
+      done(new TransfromError(error));
     }
   }
 );
 
-export default localStrategy;
+export default LocalStrategy;
