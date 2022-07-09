ecc8c2d5b6efbf1244b1ec20a767deb80bfe2d44
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2022-07-09 16:51:06 +0700

    create page not found middleware

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,5 +1,6 @@
 import { MODE } from "../config/env.config.js";
 import BaseError from "../helpers/apiError.helper.js";
+import { TransfromError } from "../helpers/baseError.helper.js";
 import consoleLog from "../utils/consoleLog.js";
 import httpStatusCodes from "../utils/httpStatusCode.js";
 
@@ -15,32 +16,42 @@ function logErrorMiddleware(err, req, res, next) {
   next(err);
 }
 
+function return404(req, res, next) {
+  const error = new TransfromError(
+    new BaseError("BAD_REQUEST", 404, "Page Not Found", false, {
+      errorView: "errors/404",
+      renderData: {
+        title: "Page Not Found",
+      },
+      responseType: "page",
+    })
+  );
+  return next(error);
+}
+
 function returnError(err, req, res, next) {
   const message = err.message;
   const status = err.statusCode || httpStatusCodes.INTERNAL_SERVER;
-  const type = err.responseType;
+  const type = req.responseType;
   const stack = MODE == "development" ? err.stack : null;
 
   if (type == "json") {
     return res.status(status).json({ ...err, stack });
-  } else if (type == "page") {
-    const view = err?.errorView || "errors/500";
-    const errData = {
-      message: message,
-      ...err,
-    };
-    const renderData = err?.renderData || null;
-
-    return res.status(status).render(view, {
-      errors: errData,
-      ...renderData,
-      stack,
-    });
-  } else {
-    res
-      .status(status)
-      .json({ ...err, stack: MODE == "development" ? err.stack : null });
   }
+
+  const view = err?.errorView || "errors/500";
+  const errData = {
+    message: message,
+    ...err,
+  };
+
+  const renderData = err?.renderData || { title: message, path: "/error" };
+
+  return res.status(status).render(view, {
+    errors: errData,
+    ...renderData,
+    stack,
+  });
 }
 
 function isOperationalError(error) {
@@ -50,4 +61,10 @@ function isOperationalError(error) {
   return false;
 }
 
-export { logError, logErrorMiddleware, returnError, isOperationalError };
+export {
+  logError,
+  logErrorMiddleware,
+  returnError,
+  isOperationalError,
+  return404,
+};
