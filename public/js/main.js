cdcb97c039ca7aa50469f9fd1ec18afbdc94f1ee
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2022-07-10 19:39:10 +0700

    config template toastr

--- public/js/main.js
+++ public/js/main.js
@@ -5,9 +5,23 @@ const formLogout = document.getElementById("form-logout");
 logoutActions.forEach(el => {
   el.addEventListener("click", event => {
     event.preventDefault();
-
     if (formLogout) {
       console.log(formLogout.submit());
     }
   });
 });
+
+const ToastsTemplate = [...document.querySelectorAll("#toast-template")];
+if (ToastsTemplate.length !== 0) {
+  ToastsTemplate.map(toast => {
+    const type = toast.dataset.type;
+    const message = toast.dataset.message;
+    const title = toast.dataset.title;
+    toastr[type](message, title, {
+      closeButton: true,
+      timeOut: 8000,
+      progressBar: true,
+      tapToDismiss: false,
+    });
+  });
+}
