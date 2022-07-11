6c5964b794b4dfa669e16c29fbe6a63a1bbac405
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2022-07-11 17:02:19 +0700

    [FIX] not found voucher routes

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -2,6 +2,7 @@ import { ensureAuth } from "../middleware/auth.js";
 import CategoryRoutes from "../modules/category/category.routes.js";
 import NominalRoutes from "../modules/nominal/nominal.routes.js";
 import UserRoutes from "../modules/user/user.routes.js";
+import VoucherRoutes from "../modules/voucher/voucher.routes.js";
 
 function MainRoutes(app) {
   app.get("/", ensureAuth, (req, res) => {
@@ -12,10 +13,9 @@ function MainRoutes(app) {
   });
 
   UserRoutes(app);
-
   CategoryRoutes(app);
-
   NominalRoutes(app);
+  VoucherRoutes(app);
 }
 
 export default MainRoutes;
