dd1159c139a3da4dbb2cb76e972752c2cb322db6
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2022-07-18 17:14:19 +0700

    add folder path param in uploadSingleImage() middleware

--- src/middleware/upload.js
+++ src/middleware/upload.js
@@ -1,48 +1,50 @@
 import multer from "multer";
 import { UPLOAD_PATH } from "../config/env.config.js";
-
 import Upload from "../helpers/upload.helper.js";
 
-const uploadSingle = new Upload({
-  fieldName: "image",
-  folderName: UPLOAD_PATH + "/vouchers",
-}).single();
+export const uploadSingleImage =
+  (folderPath = UPLOAD_PATH) =>
+  (req, res, next) => {
+    const uploadSingle = new Upload({
+      fieldName: "image",
+      folderName: UPLOAD_PATH + folderPath,
+      filename: req.body.filename,
+    }).single();
 
-export const uploadSingleImage = (req, res, next) => {
-  uploadSingle(req, res, function (err) {
-    let file = {
-      type: "success",
-      message: "Upload file success",
-      data: req.file,
-    };
-    if (err instanceof multer.MulterError) {
-      file = {
-        type: "error",
-        message: "Failed to upload",
-        data: null,
+    uploadSingle(req, res, function (err) {
+      let file = {
+        type: "success",
+        message: "Upload file success",
+        data: req.file,
       };
-      req.fileimg = file;
-      next();
-    } else if (err) {
-      file = {
-        type: "error",
-        message: "Failed to upload",
-        data: null,
-      };
-      req.fileimg = file;
-      next();
-    } else {
-      if (req.file === undefined) {
+      if (err instanceof multer.MulterError) {
         file = {
-          type: "warning",
-          message: "Please upload your file",
+          type: "error",
+          message: "Failed to upload",
           data: null,
         };
+        req.fileimg = file;
+        next();
+      } else if (err) {
+        file = {
+          type: "error",
+          message: "Failed to upload",
+          data: null,
+        };
+        req.fileimg = file;
+        next();
+      } else {
+        if (req.file === undefined) {
+          file = {
+            type: "warning",
+            message: "Please upload your file",
+            data: null,
+          };
+        }
+        req.fileimg = file;
+        next();
       }
-      req.fileimg = file;
-      next();
-    }
-  });
-};
+    });
+  };
 
 // export default uploadFilesMiddleware ;
