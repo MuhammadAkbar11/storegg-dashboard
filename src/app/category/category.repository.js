e893e109aee2e12bdf034d8e9166f73c592ab9d1
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2022-07-30 22:28:14 +0700

    refactor: implentation sequelize in category's repository functions

--- src/app/category/category.repository.js
+++ src/app/category/category.repository.js
@@ -1,57 +1,61 @@
 import { TransfromError } from "../../helpers/baseError.helper.js";
-import CategoryModel from "./category.model.js";
+import Logger from "../../helpers/logger.helper.js";
+import Category from "../../models/category.model.js";
 
-export const findAllCategories = async () => {
+export const findAllCategories = async filter => {
   try {
-    const data = await CategoryModel.find({});
+    const data = await Category.findAll({ ...filter });
     return data;
   } catch (error) {
-    console.error("[EXCEPTION] findOneUser", error);
+    Logger.error(error, "[EXCEPTION] findAllCategories");
     throw new TransfromError(error);
   }
 };
 
 export const findCategoryById = async id => {
   try {
-    const data = await CategoryModel.findById(id);
-
+    const data = await Category.findByPk(id);
     return data;
   } catch (error) {
-    console.error("[EXCEPTION] findOneUser", error);
+    Logger.error(error, "[EXCEPTION] findCategoryById");
     throw new TransfromError(error);
   }
 };
 
 export const createCategory = async data => {
   try {
-    const result = await CategoryModel.create({ ...data });
+    const result = await Category.create({ ...data });
     return result;
   } catch (error) {
-    console.error("[EXCEPTION] createCategory", error);
+    Logger.error(error, "[EXCEPTION] createCategory");
     throw new TransfromError(error);
   }
 };
 
 export const updateCategory = async (id, data) => {
   try {
-    const category = await CategoryModel.findById(id);
+    const category = await Category.findByPk(id);
 
     category.name = data.name;
     category.description = data.description;
 
     return await category.save();
   } catch (error) {
-    console.error("[EXCEPTION] createCategory", error);
+    Logger.error(error, "[EXCEPTION] updateCategory");
     throw new TransfromError(error);
   }
 };
 
 export const deleteCategoryById = async id => {
   try {
-    const result = await CategoryModel.deleteOne({ _id: id });
+    const result = await Category.destroy({
+      where: {
+        category_id: id,
+      },
+    });
     return result;
   } catch (error) {
-    console.error("[EXCEPTION] createCategory", error);
+    Logger.error(error, "[EXCEPTION] deleteCategoryById");
     throw new TransfromError(error);
   }
 };
