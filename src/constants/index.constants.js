54c13763009bda22596c470a244e0b9158e30291
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2022-07-31 21:39:33 +0700

    fix: Dev static folder

--- src/constants/index.constants.js
+++ src/constants/index.constants.js
@@ -42,7 +42,7 @@ export const TABLE_AUTO_INCREMENT = {
 };
 
 export const STATIC_FOLDER = path.join(__dirname, "public");
-export const DEV_STATIC_FOLDER = path.join(__dirname, ".dev", "public");
+export const DEV_STATIC_FOLDER = path.join(__dirname, ".dev");
 export const ROOT_FOLDER = __dirname;
 export const DEFAULT_THUMBNAIL = "/uploads/Default-Thumbnail.png";
 export const DEFAULT_USER_PP = "/uploads/Default-Avatar-1.jpg";
