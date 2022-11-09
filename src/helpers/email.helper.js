f613c441a5162a16c6732fbd7bd2c6678f5c6bec
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2022-11-09 15:46:16 +0700

    refactor: adding logger into email helper func

--- src/helpers/email.helper.js
+++ src/helpers/email.helper.js
@@ -1,11 +1,21 @@
 import createTransporter from "../config/nodemailer.config.js";
 import BaseError from "./baseError.helper.js";
+import Logger from "./logger.helper.js";
+import fs from "fs";
 
 const SendEmail = async emailOptions => {
   try {
     let emailTransporter = await createTransporter();
-    await emailTransporter.sendMail(emailOptions);
+    return await emailTransporter.sendMail(emailOptions);
+
+    //     transport.sendMail(mailOptions, (error, info) => {
+    //       if (error) {
+    //           return console.log(error);
+    //       }
+    //       console.log('Message sent: %s', info.messageId);
+    // });
   } catch (err) {
+    Logger.error(err);
     const errors = new BaseError(
       "MAILER_ERROR",
       err.statusCode,
