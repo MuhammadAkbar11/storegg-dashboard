cc5328a3ab70afe9f3f8b02c02d59283b81f8d3f
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2022-11-15 08:04:03 +0700

    fix: user login session

--- src/config/sessionsStore.config.js
+++ src/config/sessionsStore.config.js
@@ -1,14 +1,40 @@
 import session from "express-session";
+import { DataTypes } from "sequelize";
 import sequelizeSession from "connect-session-sequelize";
 import MySQLConnection from "./db.config.js";
-
+import Logger from "../helpers/logger.helper.js";
 const SequelizeStore = sequelizeSession(session.Store);
 
+// MySQLConnection
+MySQLConnection.define(
+  "Session",
+  {
+    sid: {
+      type: DataTypes.STRING,
+      primaryKey: true,
+    },
+    user_id: DataTypes.STRING,
+    expires: DataTypes.DATE,
+    data: DataTypes.TEXT,
+  },
+  {
+    tableName: "gg_sessions",
+  }
+);
+
+function extendDefaultFields(defaults, session) {
+  Logger.info(`[SESSION] Current User is ${session?.passport?.user || "NULL"}`);
+  return {
+    data: defaults.data,
+    expires: defaults.expires,
+    user_id: session?.passport?.user,
+  };
+}
+
 const sessionStore = new SequelizeStore({
-  checkExpirationInterval: 900000,
-  expiration: 86400000,
   db: MySQLConnection,
   tableName: "gg_sessions",
+  extendDefaultFields: extendDefaultFields,
 });
 
 export default sessionStore;
